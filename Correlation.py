ba1d1181e1c26dea1cf48e3eaaf87efa933fa05c
Pex2892 <Pex2892@users.noreply.github.com>
2021-03-02 14:39:16 +0100

    Create Correlation.py

---
+++ Correlation.py
@@ -0,0 +1,18 @@
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+class Correlation:
+    def matrix_corr(self, df, methods: list=['pearson'], show=True, figname='corrmatrix.png', figsize=(10, 5)):
+        for method in methods:
+            corrMatrix = df.corr(method=method)
+            plt.figure(figsize=figsize)
+            sns.heatmap(corrMatrix, cmap='coolwarm', linewidths=1.0, annot=True)
+            plt.title(f'Correlation matrix with {method.capitalize()} correlation coeff.')
+            plt.tight_layout()
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig(os.path.join(os.getcwd(), 'results', figname))
