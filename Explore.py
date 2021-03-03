bec73b868c72980b557f5635b3073c17239552c7
Pex2892 <Pex2892@users.noreply.github.com>
2021-03-03 18:46:19 +0100

    Fix cmap and title in Explore

--- Explore.py
+++ Explore.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from matplotlib import colors
 import matplotlib.pyplot as plt
 
 
@@ -14,18 +15,27 @@ class Explore:
 
         # Create main figure
         fig = plt.figure(1, figsize=figsize)
-        fig.suptitle(f'{title}{data[0][0].shape[0]} Samples')
+        if tot_subplots > 1:
+            fig.suptitle(f'{title}{data[0][0].shape[0]} Samples')
+        else:
+            fig.suptitle(f'{data[0][1]} ({data[0][0].shape[0]} Samples)')
+
         for k in range(tot_subplots):
             # add every single subplot to the figure with a for loop
             ax = fig.add_subplot(Rows, tot_columns, Position[k])
 
             height = data[k][0].value_counts().to_numpy()
             bars = data[k][0].unique()
+
+            cmap = plt.cm.get_cmap('tab20', len(bars))
+            c = [colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
+
             y_pos = np.arange(len(bars))
-            ax.bar(y_pos, height)
+            ax.bar(y_pos, height, color=c)
             ax.set_xticks(y_pos)
             ax.set_xticklabels(bars)
-            ax.set_title(data[k][1])
+            if tot_subplots > 1:
+                ax.set_title(data[k][1])
             ax.set_ylabel(data[k][3])
             ax.set_xlabel(data[k][2])
             ax.grid(axis='y', linestyle='--')
@@ -36,6 +46,7 @@ class Explore:
             plt.show()
         else:
             plt.savefig(os.path.join(os.getcwd(), 'results', figname))
+        plt.close()
 
     def multiple_barplot_horizontal(self, data: list, tot_subplots: int, tot_columns: int, title: str, show=True, figname='multiplebarplot.png', figsize=(10, 5)):
         # data = [numpy_array, title subplot, xlabel, ylabel, xticks_step]
@@ -49,19 +60,28 @@ class Explore:
 
         # Create main figure
         fig = plt.figure(1, figsize=figsize)
-        fig.suptitle(f'{title}{data[0][0].shape[0]} Samples')
+        if tot_subplots > 1:
+            fig.suptitle(f'{title}{data[0][0].shape[0]} Samples')
+        else:
+            fig.suptitle(f'{data[0][1]} ({data[0][0].shape[0]} Samples)')
+
         for k in range(tot_subplots):
             # add every single subplot to the figure with a for loop
             ax = fig.add_subplot(Rows, tot_columns, Position[k])
 
             height = data[k][0].value_counts().to_numpy()
             bars = data[k][0].unique()
+
+            cmap = plt.cm.get_cmap('tab20c', len(bars))
+            c = [colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
+
             y_pos = np.arange(len(bars))
-            ax.barh(y_pos, height)
+            ax.barh(y_pos, height, color=c)
             ax.set_xticks(np.arange(0, height.max()+1, data[k][4]))
             ax.set_yticks(y_pos)
             ax.set_yticklabels(bars)
-            ax.set_title(data[k][1])
+            if tot_subplots > 1:
+                ax.set_title(data[k][1])
             ax.set_ylabel(data[k][3])
             ax.set_xlabel(data[k][2])
 
@@ -73,3 +93,4 @@ class Explore:
             plt.show()
         else:
             plt.savefig(os.path.join(os.getcwd(), 'results', figname))
+        plt.close()
