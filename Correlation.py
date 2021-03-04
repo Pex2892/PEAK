80b80df43297a3b2e9e9035304e86252d050bdeb
Pex2892 <Pex2892@users.noreply.github.com>
2021-03-04 15:33:41 +0100

    Fix save of the correlation matrix

--- Correlation.py
+++ Correlation.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 
 
 class Correlation:
-    def matrix_corr(self, df, methods: list=['pearson'], show=True, figname='corrmatrix.png', figsize=(10, 5)):
+    def matrix_corr(self, df, methods: list=['pearson'], show=True, figsize=(10, 5)):
         for method in methods:
             corrMatrix = df.corr(method=method)
             plt.figure(figsize=figsize)
             sns.heatmap(corrMatrix, cmap='GnBu', linewidths=1.0, annot=True)
             plt.title(f'Correlation matrix with {method.capitalize()} correlation coeff.')
             plt.tight_layout()
+            if show:
+                plt.show()
+            else:
+                plt.savefig(os.path.join(os.getcwd(), 'results', f'corrMatrix_{method}.png'))
 
-        if show:
-            plt.show()
-        else:
-            plt.savefig(os.path.join(os.getcwd(), 'results', figname))
+        plt.close()
