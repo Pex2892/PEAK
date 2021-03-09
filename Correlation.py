22b3092bfeebadfa5ddb8d10e9e9fb41318d6cae
Pex2892 <Pex2892@users.noreply.github.com>
2021-03-09 18:48:25 +0100

    Correlation automation

--- Correlation.py
+++ Correlation.py
@@ -1,10 +1,56 @@
 import os
+import re
+import numpy as np
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
+from itertools import combinations
+from matplotlib import colors
+import multiprocessing as mlp
+from joblib import Parallel, delayed
 
 
 class Correlation:
-    def matrix_corr(self, df, methods: list=['pearson'], show=True, figsize=(10, 5)):
+
+    def automate(self, obj):
+        cols = obj.dataset.select_dtypes(include=['int64', 'float64']).columns.values
+        cc = list(combinations(cols, 2))
+
+        df_corr = pd.DataFrame(columns=['columns', 'pearson', 'spearman', 'kendall'])
+
+        r = Parallel(n_jobs=mlp.cpu_count(), verbose=10)(delayed(self.corr_2_col)(obj.dataset, list(c), None, False, (8, 5)) for c in cc)
+        df_corr = df_corr.append(r, ignore_index=True)
+
+        self.matrix_corr(obj.dataset, ['pearson', 'spearman', 'kendall'], show=False, figsize=(15, 7))
+
+        df_corr.to_csv(os.path.join(os.getcwd(), 'results', 'correlation', 'All_correlations.csv'), index=False, header=True, sep='\t', encoding='utf-8')
+
+    def corr_2_col(self, df, cols, title: str = None, show: bool = True, figsize: tuple = (10, 5)):
+        scores = [df[cols].corr(method='pearson').iloc[0, 1], df[cols].corr(method='spearman').iloc[0, 1],
+                  df[cols].corr(method='kendall').iloc[0, 1]]
+
+        if not np.isnan(scores).any() and len(set(scores)) != 1:
+            plt.figure(figsize=figsize)
+            cmap = plt.cm.get_cmap('GnBu', 4)
+            c_list = [colors.rgb2hex(cmap(i)) for i in range(1, cmap.N)]
+
+            plt.bar(['Pearson', 'Spearman', 'Kendall'], scores, color=c_list)
+
+            if title is None:
+                plt.title(f'Corr: {cols[0]} and {cols[1]}')
+            else:
+                plt.title(title)
+
+            if show:
+                plt.show()
+            else:
+                plt.savefig(os.path.join(os.getcwd(), 'results', 'correlation', f'corr_{"".join(re.split("[^a-zA-Z]*", cols[0]))}_{"".join(re.split("[^a-zA-Z]*", cols[1]))}.png'))
+
+        row = {'columns': ' - '.join(cols), 'pearson': scores[0], 'spearman': scores[1], 'kendall': scores[2]}
+
+        return row
+
+    def matrix_corr(self, df, methods: list=['pearson'], show: bool = True, figsize=(10, 5)):
         for method in methods:
             corrMatrix = df.corr(method=method)
             plt.figure(figsize=figsize)
@@ -14,6 +60,6 @@ class Correlation:
             if show:
                 plt.show()
             else:
-                plt.savefig(os.path.join(os.getcwd(), 'results', f'corrMatrix_{method}.png'))
+                plt.savefig(os.path.join(os.getcwd(), 'results', 'correlation', f'corr_matrix_{method}.png'))
 
         plt.close()
