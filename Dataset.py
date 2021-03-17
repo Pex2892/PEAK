1582408ddc766a95b396a61571ab26fc4363785b
Pex2892 <Pex2892@users.noreply.github.com>
2021-03-17 17:09:28 +0100

    Add drop_rows method
    Added comments and prints

--- Dataset.py
+++ Dataset.py
@@ -5,12 +5,15 @@ from sklearn.model_selection import train_test_split
 
 
 class Dataset:
-    def __init__(self, f: str, sep: str = '', skiprows: int = 0):
+    def __init__(self, settings):
         self.dataset = None
-        self.filename = f
+        self.filename = settings['filename']
         self.type_db = os.path.splitext(os.path.join(os.getcwd(), 'dataset', self.filename))[1]
-        self.sep = sep
-        self.skiprows = skiprows
+        self.sep = settings['sep']
+        self.skiprows = settings['skiprows']
+
+        self.load()
+        print(f'>>> The dataset "{self.filename}" has been successfully loaded')
 
     def load(self):
         if os.path.exists(os.path.join(os.getcwd(), 'dataset', self.filename)):
@@ -38,15 +41,24 @@ class Dataset:
             # Drop specified labels from columns.
             self.dataset = self.dataset.drop(cols_to_del, axis=1)
 
-            # Drop the rows where at least one element is missing.
-            self.dataset = self.dataset.dropna()
+            print(f">>> Removed the following columns: {', '.join(cols_to_del)}")
         else:
             if len(cols) > 0:
                 self.dataset = self.dataset.drop(cols, axis=1)
-
+                print(f">>> Removed the following columns: {', '.join(cols)}")
             else:
                 print('Error: No column to delete was found')
                 exit()
+
+    def drop_rows(self, rows: list = None):
+        if rows is None:
+            # Drop the rows where at least one element is missing.
+            self.dataset = self.dataset.dropna()
+            print('>>> Drop the rows where at least one element is missing')
+        else:
+            if len(rows) > 0:
+                self.dataset = self.dataset.drop(rows, axis=0)
+                print(f">>> Removed the following rows by index: {rows}")
 
     def normalize(self, cols: list = None):
         min_max_scaler = MinMaxScaler()
@@ -57,8 +69,9 @@ class Dataset:
 
         if len(cols) > 0:
             self.dataset[cols] = min_max_scaler.fit_transform(self.dataset[cols])
+            print(f">>> Columns were normalized between 0 and 1: {', '.join(cols)}")
         else:
-            print('Error: No column to normalize was found')
+            print('Error: No columns to normalize was found')
             exit()
 
     def categorial_to_numeric(self, cols: list = None, newcols: list = None):
@@ -70,10 +83,8 @@ class Dataset:
         if len(cols) == len(newcols):
             for c, nc in zip(cols, newcols):
                 self.dataset.insert(self.dataset.columns.get_loc(c)+1, nc, pd.factorize(self.dataset[c])[0])
-
-            # for c, nc in zip(cols, newcols):
-            #     self.dataset[nc], unique = pd.factorize(self.dataset[c])
-
+            print(f">>> The following categorical columns were coded into numbers: {', '.join(cols)}")
+            print(f">>> The number columns are named as follows: {', '.join(newcols)}")
         else:
             print('The lengths between "cols" and "newcols" are not the same')
             exit()
@@ -86,3 +97,4 @@ class Dataset:
     def export_csv(self):
         self.dataset.to_csv(os.path.join(os.getcwd(), 'results', 'dataset_processed.csv'), index=False,
                             header=True, sep='\t', encoding='utf-8')
+        print(f">>> A backup of the dataset has been created in the following path: {os.path.join(os.getcwd(), 'results', 'dataset_processed.csv')}")
